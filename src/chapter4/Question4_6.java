20428e5b35b768308a8a659e9c5eec0c5337dbba
yongrongwang <2278508989@qq.com>
2019-06-05 11:02:03 +0800

    first commit

--- src/chapter4/Question4_6.java
+++ src/chapter4/Question4_6.java
@@ -1,5 +1,7 @@
 package chapter4;
 
+import ctciLibrary.TreeNode;
+
 /**
  * @author yr
  * 请设计一个算法，寻找二叉树中指定结点的下一个结点（即中序遍历的后继）。给定树的根结点指针TreeNode* root和结点的值int p，请返回值为p的结点
@@ -7,19 +9,41 @@ package chapter4;
  */
 public class Question4_6 {
 
+    static TreeNode pre = new TreeNode(-1);
+
     /**
-     * 
+     * 采用递归方法中序遍历，使用一个指针pre指向前一个节点，如果前一个节点的数值等于p，则返回当前节点的数值，否则pre指向root，继续递归遍历
      * @param root 根结点
      * @param p 结点的值
      * @return 后继结点的值
      */
     public static int findSucc(TreeNode root, int p) {
-
+        if (root == null) {
+            return -1;
+        }
+        // 寻找最左边的节点
+        int left = findSucc(root.left, p);
+        // 找到节点，则直接返回值
+        if (left != -1) {
+            return left;
+        }
+        // 如果前一个节点是要找的节点，则返回当前节点的数值
+        if (pre.val == p) {
+            return root.val;
+        }
+        // 如果前一个节点不是要找的节点，则pre指向root
+        pre = root;
+        return findSucc(root.right, p);
     }
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-
+        int[] array = TreeNode.sortedArray(20);
+        TreeNode root = TreeNode.createMinimalBST(array, 0, array.length - 1);
+        for (int i = 0; i < array.length; i++) {
+            int succ = findSucc(root, i);
+            System.out.println(i + "->" + (succ == -1 ? null : succ));
+        }
     }
 
 }
---
+++ src/chapter4/Question4_7.java
@@ -0,0 +1,33 @@
+package chapter4;
+
+import ctciLibrary.AsSortedMethods;
+
+/**
+ * @author yr
+ * 有一棵无穷大的满二叉树，其结点按根结点一层一层地从左往右依次编号，根结点编号为1。现在有两个结点a，b。请设计一个算法，求出a和b点的最近公共祖
+ * 先的编号。给定两个int a,b。为给定结点的编号。请返回a和b的最近公共祖先的编号。注意这里结点本身也可认为是其祖先。
+ */
+public class Question4_7 {
+
+    public static int getLCA(int a, int b) {
+        while (a != b) {
+            if (a > b) {
+                a = a / 2;
+            } else if (b > a) {
+                b = b / 2;
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            int a = AsSortedMethods.randomIntInRange(1, n);
+            int b = AsSortedMethods.randomIntInRange(1, n);
+            System.out.println("a = " + a + ", " + "b = " + b + ", ancestor = " + getLCA(a, b));
+        }
+    }
+
+}
