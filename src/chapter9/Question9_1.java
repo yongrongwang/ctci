59314da66747e121dbd82b2efc9cbcc7283d6d47
yongrongwang <2278508989@qq.com>
2019-06-19 19:06:06 +0800

    first commit

--- src/chapter9/Question9_1.java
+++ src/chapter9/Question9_1.java
@@ -6,18 +6,43 @@ package chapter9;
  * Mod 1000000007给定一个正整数int n，请返回一个数，代表上楼的方式数。保证n小于等于100000。
  */
 public class Question9_1 {
+
     /**
-     * 
+     * 用一个数组保存中间结果，空间换时间，递归变迭代，然后将数组用三个变量代替
      * @param n
      * @return
      */
     public static int countWays(int n) {
-
+        int x = 1000000007;
+        // 存储中间结果的三个变量
+        int[] a = { 1, 2, 4 };
+        if (n <= 0)
+            return 0;
+        if (n == 1)
+            return a[0];
+        if (n == 2)
+            return a[1];
+        if (n == 3)
+            return a[2];
+        for (int i = 4; i <= n; i++) {
+            int t = ((a[0] + a[1]) % x + a[2]) % x;
+            a[0] = a[1];
+            a[1] = a[2];
+            a[2] = t;
+        }
+        return a[2];
     }
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-
+        int n = 36196;
+        for (int i = 1; i <= 36196; i++) {
+            long t1 = System.currentTimeMillis();
+            int c1 = countWays(i);
+            long t2 = System.currentTimeMillis();
+            long d1 = t2 - t1;
+            System.out.println(i + ": " + "c1 = " + c1 + ", d1 = " + d1 + " ");
+        }
     }
 
 }
